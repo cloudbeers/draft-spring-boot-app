d04bcf866d0e1625afaa7e699f4f92ccb43a7673
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-01-31 10:44:06 +0100

    Initial commit

---
+++ src/main/java/com/cloudbees/test/kubernetes/draft/DraftSpringBootAppSpringBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.cloudbees.test.kubernetes.draft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftSpringBootAppSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftSpringBootAppSpringBootAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cloudbees/test/kubernetes/draft/DraftSpringBootAppSpringBootAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloudbees.test.kubernetes.draft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DraftSpringBootAppSpringBootAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
